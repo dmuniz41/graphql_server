ab13846dc0fe9870f1411f932b12adf72aaa5c3c
Daniel Muniz <danielnubiz902@gmail.com>
2024-01-08 00:15:36 -0500

    Add resolvers

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 import { typeDefs } from "./schema";
 
+const resolvers = {
+  
+}
+
 //* Server setup
 
 const server = new ApolloServer({
