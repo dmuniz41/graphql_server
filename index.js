9d7a403d78a2122ce2747f1e25a7b43c8b9bef42
Daniel Muniz <danielnubiz902@gmail.com>
2024-01-10 11:31:28 -0500

    Add queries with params

--- index.js
+++ index.js
@@ -14,14 +14,22 @@ const resolvers = {
     authors() {
       return db.authors;
     },
+    review(_, args) {
+      return db.reviews.find((review) => review.id === args.id);
+    },
+    game(_, args) {
+      return db.games.find((game) => game.id === args.id);
+    },
+    author(_, args) {
+      return db.authors.find((author) => author.id === args.id);
+    },
   },
 };
 
 //* Server setup
-
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
 });
 
 const { url } = await startStandaloneServer(server, {
