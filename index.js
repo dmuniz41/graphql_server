2eeea12e3dcaf0c9f0eab08ab6c7776a647db5b5
Daniel Muniz <danielnubiz902@gmail.com>
2024-01-10 10:52:50 -0500

    Add resolvers

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
-import { typeDefs } from "./schema";
+import { typeDefs } from "./schema.js";
+import db from "./_db.js";
 
 const resolvers = {
-  
-}
+  Query: {
+    games() {
+      return db.games;
+    },
+    reviews() {
+      return db.reviews;
+    },
+    authors() {
+      return db.authors;
+    },
+  },
+};
 
 //* Server setup
 
 const server = new ApolloServer({
   typeDefs,
+  resolvers
 });
 
 const { url } = await startStandaloneServer(server, {
